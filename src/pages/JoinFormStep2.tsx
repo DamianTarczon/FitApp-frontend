a3ac945f747be2657872342550a0be1808f61a05
Damian <tarczon96@gmail.com>
2022-12-16 12:54:51 +0100

    Step 2 - basic form added

--- src/pages/JoinFormStep2.tsx
+++ src/pages/JoinFormStep2.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import {
+  setAge,
+  setHeight,
+  setWeight,
+  selectForm,
+} from './JoinFormSlice';
+import BigTile from '../components/BigTile';
+import './JoinFormStep2.scss';
 
 export default function JoinFormStep2() {
+  const form = useAppSelector(selectForm);
+  const dispatch = useAppDispatch();
+
+  const pictureWithText = <div>Tutaj będzie zdjęcie i tekst</div>
+  const basicForm = 
+  <div>
+    <form>
+      <input 
+        type='text'
+        placeholder='Wiek'
+        onChange={(e) => dispatch(setAge(e.target.value))}
+      />
+      <input 
+        type='text'
+        placeholder='Wzrost'
+        onChange={(e) => dispatch(setHeight(e.target.value))}
+      />
+      <input 
+        type='text'
+        placeholder='Waga'
+        onChange={(e) => dispatch(setWeight(e.target.value))}
+      />
+    </form>
+    <button>Przejdź dalej</button>
+  </div>
+
   return (
-    <div>Step2</div>
+    <div className="form-container">
+      <BigTile content={pictureWithText} color="orange"/>
+      <BigTile content={basicForm} color="white"/>
+    </div>
   );
 }
-
