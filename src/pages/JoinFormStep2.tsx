a28893803dccbc422ce2bdb96a5c4a71bdf2f15c
Damian <tarczon96@gmail.com>
2022-12-16 12:59:06 +0100

    Step2 - added Link with href to button

--- src/pages/JoinFormStep2.tsx
+++ src/pages/JoinFormStep2.tsx
@@ -8,6 +8,7 @@ import {
 } from './JoinFormSlice';
 import BigTile from '../components/BigTile';
 import './JoinFormStep2.scss';
+import { Link } from "react-router-dom";
 
 export default function JoinFormStep2() {
   const form = useAppSelector(selectForm);
@@ -33,7 +34,7 @@ export default function JoinFormStep2() {
         onChange={(e) => dispatch(setWeight(e.target.value))}
       />
     </form>
-    <button>Przejdź dalej</button>
+    <Link to='/join-form-step-3'><button>Przejdź dalej</button></Link>
   </div>
 
   return (
