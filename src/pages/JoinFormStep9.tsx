6e94045781c909463713d2a052d904ee7e1454a1
Kamil Kornek <kamil.kornek96@gmail.com>
2022-12-19 13:17:21 +0100

    changle results display

--- src/pages/JoinFormStep9.tsx
+++ src/pages/JoinFormStep9.tsx
@@ -2,28 +2,17 @@ import { useAppSelector } from '../app/hooks';
 import {
   selectForm,
 } from '../reducers/formReducer';
-import './JoinFormStep9.scss';
+import { Link } from "react-router-dom";
 
 
 export default function JoinFormStep9() {
   const form = useAppSelector(selectForm);
 
-  const allergensList = form.form.allergens.map(a => a + ', ')
-
-  const notLikedProductsList = form.form.notLikedProducts.map(a => a + ', ')
 
   return (
     <div className="form-results">
-      <p>Płeć: {form.form.gender}</p>
-      <p>Wiek: {form.form.age}</p>
-      <p>Wzrost: {form.form.height}</p>
-      <p>Waga: {form.form.weight}</p>
-      <p>Typ diety: {form.form.dietType}</p>
-      <p>Alergeny: {allergensList}</p>
-      <p>Poziom aktywności: {form.form.activityLevel}</p>
-      <p>Liczba posiłków: {form.form.numberOfMeals}</p>
-      <p>Niechciane produkty: {notLikedProductsList}</p>
-      <p>Choroby: {form.form.disease}</p>
+      <pre>{JSON.stringify(form.form,null, 2)}</pre>
+      <Link to="/join-form-step-1">Start over</Link>
     </div>
   );
 }
