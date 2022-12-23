35e3680519ea40f69d64956bc92c53f5f344d787
Damian <tarczon96@gmail.com>
2022-12-23 19:12:52 +0100

    TypeButton icons src added

--- src/components/TypeButton/index.tsx
+++ src/components/TypeButton/index.tsx
@@ -1,12 +1,18 @@
 import './index.scss';
-import { noSelectedIcon, selectedIcon, femaleIcon, maleIcon } from '../../assets/svg_icons';
 
 export default function TypeButton(props: any) {
+  const maleIcon: string = '/assets/icons/male-icon.svg'
+  const femaleIcon: string = '/assets/icons/female-icon.svg'
+  const selectedIcon: string = '/assets/icons/selected.svg'
+  const noSelectedIcon: string = '/assets/icons/no-selected.svg'
+
+
+
     return (
       <button className="select-button">
         <div className='icon-with-select'>
-            <span className="icon">{props.value === 'male' ? maleIcon : femaleIcon}</span>
-            <span className="selected-icon">{props.selected ? selectedIcon : noSelectedIcon}</span>
+            <span className="icon"><img src={props.value === 'male' ? maleIcon : femaleIcon}/></span>
+            <span className="selected-icon"><img src={props.selected ? selectedIcon : noSelectedIcon}/></span>
         </div>
         <div className="text">{props.text}</div>
       </button>
